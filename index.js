c96e722b0e709fdd22386c5d70220181b964bf34
Ania <ania.makes@gmail.com>
2018-02-02 15:34:38 +0000

    searches for a term and outputs 10 titles

--- index.js
+++ index.js
@@ -14,15 +14,75 @@ function processQuery(event){
 function fetchingFn(incoming){
   fetch(`http://www.omdbapi.com/?s=${incoming}&apikey=e5643f99`)
   .then(function(response) {
+      console.log(response);
       return response.json();
   }).then(function(data) {
       console.log(data);
       document.getElementsByClassName("first-search")[0].style.display = "none";
+      document.getElementsByClassName("not-first-search")[0].style.display = "block";
       var listOfFilms = data.Search;
       console.log(listOfFilms);
-      listOfFilms.forEach(item => console.log(item.Poster, item.Title, item.Year, item.imdbID))
+      listOfFilms.forEach(item => makeRowForInput(item));
 
   }).catch(function(error) {
     debugger;
   });
+}
+
+function makeRowForInput(movie){
+  var posterSrc = movie.Poster;
+  var movieTitle = movie.Title;
+  var movieYear = movie.Year;
+  var movieIMDBid = movie.imdbID;
+
+  var outputElementParent = document.getElementsByClassName("resultsOutput")[0];
+
+  console.log(posterSrc, movieTitle);
+
+  var movieDetails = document.createElement("div");
+  var posterBox = document.createElement("div");
+  var movieInfo = document.createElement("div");
+  var image = document.createElement("img");
+  var movieInfoTitle = document.createElement("p");
+  var movieInfoYear = document.createElement("p");
+  var movieInfoIMDB = document.createElement("p");
+  var imdbLink = document.createElement("a");
+
+  movieDetails.className = "row";
+  movieDetails.style.margin = "5px 30px";
+  posterBox.className = "col-3";
+  posterBox.align = "right";
+  movieInfo.className = "col-9";
+  image.src = posterSrc;
+  image.height = "150";
+  imdbLink.href = `http://www.imdb.com/title/${movieIMDBid}`
+  imdbLink.innerHTML = `Learn more about ${movieTitle} on IMDB`
+
+  var movieTitleContent = document.createTextNode(movieTitle);
+  var movieYearContent = document.createTextNode(movieYear);
+  var movieIMDBContent = document.createTextNode("");
+
+  movieDetails.appendChild(posterBox);
+  movieDetails.appendChild(movieInfo);
+
+  movieInfoIMDB.appendChild(imdbLink);
+
+  movieInfo.appendChild(movieInfoTitle);
+  movieInfo.appendChild(movieInfoYear);
+  movieInfo.appendChild(movieInfoIMDB);
+
+  movieInfoTitle.appendChild(movieTitleContent);
+  movieInfoYear.appendChild(movieYearContent);
+  movieInfoIMDB.appendChild(movieIMDBContent);
+
+  posterBox.appendChild(image);
+
+
+  outputElementParent.appendChild(movieDetails);
+  // resultsOutput = resultsOutput +
+// `  <div class="row eachResult">
+//     <div class="col">
+//
+//     </div>
+//   </div>`
 }
