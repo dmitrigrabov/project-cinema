b3f631a0355a5e800def55e2d60c5da69e7143c0
mathildepind <mathilde.pind.rasmussen@gmail.com>
2018-02-04 18:56:21 +0000

    small change in html file

--- fetch-movie.js
+++ fetch-movie.js
@@ -1,12 +1,47 @@
-var searchString = 'jurassic';
-var searchUrl = 'http://www.omdbapi.com/?s=jurassic&apikey=25a585bd';
+//var searchString = 'jurassic';
+//var searchUrl = 'http://www.omdbapi.com/?s=star&apikey=25a585bd';
 
-fetch(searchUrl)
+function listenForSubmit() {
+    var submitMovieSearch = document.querySelector('#submitButton');
+    submitMovieSearch.addEventListener("click", function(event) {
+      event.preventDefault();
+      var movieSearchText = document.querySelector('#searchForm').value;
+      //console.log(document.querySelector('#searchForm').value);
+
+      function setUrl() {
+        var startUrl = 'http://www.omdbapi.com/?s=';
+        var midUrl = movieSearchText.replace(/\s/g, "+").toLowerCase();
+        var endUrl = '&apikey=25a585bd';
+        var searchUrl = startUrl+midUrl+endUrl;
+        console.log(searchUrl);
+        return searchUrlTest;
+      }
+      setUrl();
+    });
+  }
+
+listenForSubmit();
+
+/*
+function setUrl() {
+  var startUrl = 'http://www.omdbapi.com/?s=';
+  var midUrl = document.querySelector('#searchForm').value;
+  var endUrl = '&apikey=25a585bd';
+  var searchUrlTest = startUrl+midUrl+endUrl;
+  console.log(searchUrlTest);
+  //return searchUrlTest;
+}
+
+setUrl();*/
+
+/*fetch(searchUrl)
   .then(function(response) {
     return response.json()
+    debugger;
   }).then(function(myJsonData) {
     const resultArray = myJsonData.Search;
+    debugger;
     resultArray.forEach(result => console.log(result.Title));
   }).catch(function(error) {
-
-  });
+    debugger;
+  });*/
