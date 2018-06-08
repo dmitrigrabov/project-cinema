9d4029220fe416f9c0b0cedcace10146fbf5a23e
Ollie Crook <olliejcrook@gmail.com>
2018-06-08 12:05:49 +0100

    add search results to results div on browser

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const getSubmit = document.querySelector("#form");
 function submitHandler(event){
   event.preventDefault();
 
+  // Capture search term
   const getMovie = document.querySelector(".searchTerm");
   const movieToSearch = getMovie.value;
 
@@ -13,10 +14,31 @@ function submitHandler(event){
       return response.json();
     }).then(function(data){
       console.log(data);
-    }).catch(function(error){
-      console.log("Sorry, I couldn't find anything. Try and search for something else.");
-    });
 
+      // Add that information to the web page.
+      const results = data.Search.map(movie =>
+          `<div class="movieCard">
+              <h2>
+                ${movie.Title}
+              </h2>
+              <p>
+                ${movie.Year}
+              </p>
+              <a href="https://www.imdb.com/title/${movie.imdbID}" target="_blank"><img src=${movie.Poster}></a>
+          </div>`
+      ).join('')
+
+      const movieEl = `
+            ${results}
+      `;
+
+      const el = document.querySelector(".results");
+
+      return el.innerHTML = movieEl;
+
+    }).catch(function(error){
+      // Do something if the search fails.
+    });
 }
 
 getSubmit.addEventListener("submit", submitHandler);
