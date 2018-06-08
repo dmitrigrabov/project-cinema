b7251e336155826b629441106e7f3e3ac9cb55c0
Ollie Crook <olliejcrook@gmail.com>
2018-06-08 10:49:39 +0100

    add ability to console.log searched movie information

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ const getSubmit = document.querySelector("#form");
 
 function submitHandler(event){
   event.preventDefault();
+
+  const getMovie = document.querySelector(".searchTerm");
+  const movieToSearch = getMovie.value;
+
+  // Search the api using movieToSearch
+  const url = `http://www.omdbapi.com/?s=${movieToSearch}&apikey=a46e4310`;
+  fetch(url)
+    .then(function(response){
+      return response.json();
+    }).then(function(data){
+      console.log(data);
+    }).catch(function(error){
+      console.log("Sorry, I couldn't find anything. Try and search for something else.");
+    });
+
 }
 
 getSubmit.addEventListener("submit", submitHandler);
