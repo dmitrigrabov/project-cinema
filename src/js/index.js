8f5b4856d8197bb12ce812f99caaac00851befb8
Ralph <ralph@rgalan.es>
2018-06-08 12:13:55 +0100

    Added template literal for movies

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,42 @@
 const omdbAPIKey = "aba7af8e";
 const form = document.querySelector("#movie-request__form");
 const movieRequest = form.querySelector("#movie-request__input");
+const resultsPlaceholder = document.querySelector("#search-results");
 
 // Movie fetch
 function movieFetch(movieRequest) {
   fetch(`http://www.omdbapi.com/?s=${movieRequest}&apikey=${omdbAPIKey}`)
     .then(response => {
-      console.warn(response);
+      // console.warn(response);
       return response.json();
-      // debugger;
     })
     .then(data => {
-      console.log(data);
-      // debugger;
+      // Create array from data results
+      const movieData = [...data.Search];
+      const movieMarkup = `
+      ${movieData
+        .map(
+          movie =>
+            // console.log(movie.Title);
+            `
+          <div class="movie-wrapper">
+            <div class="movie-poster">
+              <img src="${movie.Poster}" alt="" class="movie-poster__image">
+            </div>
+            <div class="movie-info">
+              <h2 class="movie-info__title">${movie.Title}</h2>
+              <span class="movie-info__year">${movie.Year}</span>
+              <a target="_blank" href="https://www.imdb.com/title/${
+                movie.imdbID
+              }">See on IMBD</a>
+            </div>
+          </div>
+          `
+        )
+        .join("")}
+      `;
+      resultsPlaceholder.innerHTML = movieMarkup;
+      // console.log(movieData);
     })
     .catch(error => {
       console.error(error);
