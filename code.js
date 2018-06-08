3f95731cea2fa3142c51ff9ab81c26541aa625cf
Ahmed <dulaimi89@gmail.com>
2018-06-08 13:29:09 +0100

    fetch was done and results are displayed on li under div

--- code.js
+++ code.js
@@ -1,1 +1,50 @@
-console.log("hello")
+//varibles
+const myForm = document.querySelector("form");
+const myTextArea= document.querySelector("textarea");
+const myButton = document.querySelector("button");
+const myDiv= document.querySelector("div");
+let search;
+//functions
+function fetching()
+{
+  search=myTextArea.value;
+ fetch(`http://www.omdbapi.com/?s=${search}&apikey=60ece986`)
+  .then(function(response){
+    return response.json();
+  }).then(function(jsonData){
+    displayMovies(jsonData);
+    });
+
+  /*.catch(function(error){
+    alert("error");
+  }); */
+}
+
+function displayMovies(data){
+
+  data.Search.forEach(function(movie){
+    let newNode=document.createElement("li");
+  for(let value in movie)
+  {
+    if(value=="imdbID"){
+    newNode.innerHTML+=`<a href=https://www.imdb.com/title/${movie[value]}/> LINKK </a> <br>`;
+    }
+    else if(value=="Poster"){
+      newNode.innerHTML+=`<img src=${movie[value]}> img </img> <br>`;
+    }
+    else{
+    console.log(`${value}: ${movie[value]}`);
+    newNode.innerHTML+=`${value}: ${movie[value]} <br>`;
+  }
+  }
+  myDiv.appendChild(newNode);
+});
+}
+
+function submitHandler(event){
+  event.preventDefault();
+  fetching();
+}
+
+//body
+myForm.addEventListener("submit", submitHandler);
