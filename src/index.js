d34bb31411f92bd7bdb3d44e1a067371c77cfdd7
Ralph <ralph@rgalan.es>
2018-06-08 10:51:15 +0100

    Added fetch and console.log results

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-const omdbapiKey = aba7af8e;
+const omdbAPIKey = "aba7af8e";
 const form = document.querySelector("#movie-request__form");
+const movieRequest = form.querySelector("#movie-request__input");
+
+// Movie fetch
+function movieFetch(movieRequest) {
+  fetch(`http://www.omdbapi.com/?s=${movieRequest}&apikey=${omdbAPIKey}`)
+    .then(response => {
+      console.warn(response);
+      return response.json();
+      // debugger;
+    })
+    .then(data => {
+      console.log(data);
+      // debugger;
+    })
+    .catch(error => {
+      console.error(error);
+      // debugger;
+    });
+}
 
 form.addEventListener("submit", function(e) {
   e.preventDefault();
+  movieFetch(movieRequest.value);
 });
