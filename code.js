2971c611ada07ebae8bfc26dcd9472bacf6f9fe4
Ahmed <dulaimi89@gmail.com>
2018-06-08 16:06:21 +0100

    style.css was added

--- code.js
+++ code.js
@@ -1,41 +1,42 @@
 //varibles
-const myForm = document.querySelector("form");
-const myTextArea= document.querySelector("textarea");
+const myForm = document.querySelector("Form");
+const myTextArea= document.querySelector("#searchfield");
 const myButton = document.querySelector("button");
-const myDiv= document.querySelector("div");
+const myDiv= document.querySelector(".movies");
+
 let search;
 //functions
 function fetching()
 {
+
   search=myTextArea.value;
  fetch(`http://www.omdbapi.com/?s=${search}&apikey=60ece986`)
   .then(function(response){
     return response.json();
   }).then(function(jsonData){
     displayMovies(jsonData);
-    });
-
-  /*.catch(function(error){
-    alert("error");
-  }); */
+  }).catch(function(error){
+    alert("hh")
+  });
 }
 
 function displayMovies(data){
 
   data.Search.forEach(function(movie){
     let newNode=document.createElement("li");
+    newNode.classList.add("movie");
   for(let value in movie)
   {
     if(value=="imdbID"){
-    newNode.innerHTML+=`<a href=https://www.imdb.com/title/${movie[value]}/> LINKK </a> <br>`;
+      newNode.innerHTML+=`<a href=https://www.imdb.com/title/${movie[value]}/> LINKK </a> <br>`;
     }
     else if(value=="Poster"){
-      newNode.innerHTML+=`<img src=${movie[value]}> img </img> <br>`;
+      newNode.innerHTML+=`<img src=${movie[value]}></img> <br>`;
     }
     else{
-    console.log(`${value}: ${movie[value]}`);
-    newNode.innerHTML+=`${value}: ${movie[value]} <br>`;
-  }
+      console.log(`${value}: ${movie[value]}`);
+      newNode.innerHTML+=`${value}: ${movie[value]} <br>`;
+    }
   }
   myDiv.appendChild(newNode);
 });
@@ -43,8 +44,17 @@ function displayMovies(data){
 
 function submitHandler(event){
   event.preventDefault();
+  console.log(myDiv)
+  myDiv.innerHTML="";
+
   fetching();
+}
+
+function resetHandler(){
+  console.log(myDiv)
+  myDiv.innerHTML="";
 }
 
 //body
 myForm.addEventListener("submit", submitHandler);
+myForm.addEventListener("reset", resetHandler);
