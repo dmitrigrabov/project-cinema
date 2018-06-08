5e46246fade0bf0cd3e7755d878b624524ce2687
Phoebe DG <phoebevdgash@gmail.com>
2018-06-08 16:05:20 +0100

    All primary exercises complete

--- index.js
+++ index.js
@@ -1,51 +1,80 @@
-const form = document.querySelector('#textinput');
-const input = document.querySelector('#placeholder');
-const movieInfo = document.querySelector('#movieinfo');
+const form = document.querySelector("#textinput");
+const input = document.querySelector("#placeholder");
+const movieInfo = document.querySelector("#movieinfo");
+const detailInfo = document.querySelector("#detailedinfo");
+// const movietitle = document.querySelector("#movieheading");
 
+// Event Handler "Submit"
 function createUrl(input) {
-    const search = input.value.trim();
-    return `http://www.omdbapi.com/?s=${search}&apikey=2c6457b6&`;
-}
-
-function createNode(element) {
-    return document.createElement(element);
-}
-
-function append(parent, el) {
-    return parent.appendChild(el);
+  const search = input.value.trim();
+  return `http://www.omdbapi.com/?s=${search}&apikey=2c6457b6&`;
 }
 
 function searchFilms(url) {
-    fetch(url)
-        .then(function(response) {
-            return response.json()
-        }).then(function(data) {
-            console.log(data);
-            let movies = data.Search/* .results? */;
-            return movies.map(function(movie) {
-                let div = createNode('div');
-                div.innerHTML = `
-                <h2>
-                    ${movie.Title}, ${movie.Year}
-                </h2>
-                <a href="https://www.imdb.com/title/${movie.imdbID}/">
-                    <img src="${movie.Poster}">
-                </a>
-                `;
-                append(movieInfo, div);
-            })
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
-
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      let html = data.Search.map(function(movie) {
+        return `
+          <div class="moviecard">
+            <h2 id="movieheading" class="movieheading">
+              ${movie.Title}
+            </h2>
+            <h3 class="movieyear">
+              ${movie.Year}
+            </h3>
+            <a href="https://www.imdb.com/title/${movie.imdbID}/">
+              <img src="${movie.Poster}" class="poster">
+            </a>
+          </div>
+        `;
+      }).join("");
+      movieInfo.innerHTML = html;
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
 }
 
 function submitForm(event) {
-    event.preventDefault();
-    searchFilms(createUrl(input));
+  event.preventDefault();
+  searchFilms(createUrl(input));
 }
 
-form.addEventListener('submit', submitForm);
-movieInfo.addEventListener('click', )
+// Event Handler "Click"
+function createUrl2(title) {
+  return `http://www.omdbapi.com/?t=${title}&apikey=2c6457b6&`;
+}
 
+function searchDetail(url2) {
+  fetch(url2)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+        const html = `
+          <div class="infocard">
+            <p>${data.Plot}</p> 
+          </div>
+        `;
+      detailInfo.innerHTML = html;
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+function getInfo(event) {
+  event.preventDefault();
+  if (event.target.id === "movieheading") {
+    const title = event.target.innerText;
+    searchDetail(createUrl2(title));
+  };
+}
+
+form.addEventListener("submit", submitForm);
+movieInfo.addEventListener("click", getInfo);
