f269a1d2419088da3beffc9a116413b18a563df6
Sheila HK <38942403+tigershk@users.noreply.github.com>
2018-06-10 17:54:22 +0100

    Old version

--- index.js
+++
@@ -1,9 +0,0 @@
-
-const searchStr = document.querySelector('#search_1');
-console.log(searchStr.value);
-
-// searchButton.addEventListener('click', function(event) {
-//   event.preventDefault();
-//  console.log(searchText.value);
-// });
-
