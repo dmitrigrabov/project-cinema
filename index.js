039756ef2ca194008c4cd22793bb441c07e66b9f
ytknrgn <42815334+ytknrgn@users.noreply.github.com>
2018-09-20 09:46:34 +0100

    implemented result display

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 //Get HTML element references
 const formRef = document.querySelector('.search__form');
+const resultsRef = document.querySelector('.results');
+
 
 //Add click event listeners on document
 document.addEventListener('click', event => {
@@ -12,7 +14,6 @@ document.addEventListener('click', event => {
 formRef.addEventListener('submit', event => {
     event.preventDefault();
     const searchQuery = formRef.search.value;
-    console.log(searchQuery);
     const APIQuery = `http://www.omdbapi.com/?apikey=eee5954b&s=${searchQuery}`;
     fetchResults(APIQuery);
 });
@@ -27,6 +28,16 @@ function fetchResults(APIQuery) {
     .catch(error => console.log(error));
 }
 
+//Create search result elements
 function renderResults(body) {
-    console.log(body);
+    resultsRef.innerHTML = '';
+    body.Search.forEach(item => {
+        const result = document.createElement('article');
+        result.setAttribute('class','result');
+        result.innerHTML = `<h1 class='result__title'>${item.Title}</h1>
+                            <h2 class='result__year'>${item.Year}</h2>
+                            <img class='result__poster' src=${item.Poster}>`;
+        resultsRef.appendChild(result);
+    });
+    
 }
