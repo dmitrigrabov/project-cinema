b84495ae3b83d9f6755f568074685c554c07c574
Roland Levy <rolandjlevy@gmail.com>
2018-09-21 13:57:44 +0100

    Pagination working OK for results < 100. For large numbers of results for  common words like MAN I need to find a better solution

--- src/index.js
+++ src/index.js
@@ -4,46 +4,59 @@ const apiKey = "2454706d";
 const mainNode = document.querySelector(".content");
 const form = document.querySelector(".form");
 const searchInput = document.querySelector(".search");
+// let page = document.location["page"];
 
-const getFilms = function() {
-    const urlObject = getUrlFromSearch();
+const getFilms = function(page) {
+    const urlObject = getUrlFromSearch(page);
     if (!urlObject.searchInput) return;
+    // const validPage = urlObject.page ? urlObject.page : "1";
     fetch(urlObject.url)
     .then(response => response.json())
     .then(body => {
-        listenForSearchInput();
-        showSearchResults(body, urlObject.searchInput);
+        listenForSearchInput(page);
+        showSearchResults(body, urlObject.searchInput, page);
     })
 }
 
-const showSearchResults = function (body, input) {
+const showSearchResults = function (body, input, page) {
     if (!body) return;
     mainNode.innerHTML = '';
-    let html = `<div><b><i>${body.totalResults} results for "${input}"</i></b></div><br>`;
+    const totalPageLinks = Math.floor(body.totalResults / 10) + 1;
+    let html = `<div><b><i>${body.totalResults} results for "${input}"</i></b></div>`;
+    html += `<div><i>Showing page ${page} of ${totalPageLinks}</i></div><br>`;
     body.Search.forEach(film => {
         const linkUrl = getUrlFromResultLink(film.imdbID);
         html += `<div><b>Title:</b> "${film.Title}", <b>ID:</b> <a href="${linkUrl}" target="_blank">${film.imdbID}</a></div>`;
     });
-    html += `<br><div><a href="#">Pages 1 - 10</a></div>`;
+    html += `<br><div>Page: `;
+    for (let i=1; i<=totalPageLinks; i++) {
+        html += ` <a class="page-number-link" href="#" onClick="nextPageFetch(${i})">&nbsp;${i}&nbsp;</a>`;
+        // html += (i < totalPageLinks) ? `&nbsp;&nbsp;|` : ``;
+    } 
+    html += `</div>`;
     mainNode.innerHTML = html;
 }
 
-const listenForSearchInput = function (){
+const getUrlFromSearch = function (page) {
+    if (searchInput.value.length === 0) return false;
+    const url = `${omdUrl}&s=${searchInput.value}&page=${page}&apikey=${apiKey}`;
+    return {"url": url, "searchInput": searchInput.value, "page": page};
+}
+
+const nextPageFetch = function (page){
+    getFilms(page);
+}
+
+const listenForSearchInput = function (page){
     form.addEventListener('submit', event => {
         event.preventDefault();
-        getFilms();
+        getFilms(page);
     });
 }
 
-const getUrlFromSearch = function () {
-    if (searchInput.value.length === 0) return false;
-    const url = `${omdUrl}&s=${searchInput.value}&apikey=${apiKey}`;
-    // console.log(url);
-    return {"url": url, "searchInput": searchInput.value};
-}
-
 const getUrlFromResultLink = function (id) {
-    return `${omdUrl}&i=${id}&apikey=${apiKey}`;
+    const url = `${omdUrl}&i=${id}&apikey=${apiKey}`;
+    return url;
 }
 
 const populateSelectedFilm = function (film) {
@@ -52,6 +65,5 @@ const populateSelectedFilm = function (film) {
     keys.forEach(key => {
         html += `<div><b>${key}</b>: ${film[key]}</div>`;
     });
-    // console.log(html);
 }
-listenForSearchInput();
+listenForSearchInput("1");
