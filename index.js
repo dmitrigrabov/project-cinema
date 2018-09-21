493a4178c44e61a3caa2c1f4b921eaadd386c92c
ytknrgn <42815334+ytknrgn@users.noreply.github.com>
2018-09-21 16:28:50 +0100

    improved localStorage & favorites logic

--- index.js
+++ index.js
@@ -4,27 +4,28 @@ let page = 1;
 //Initialize: HTML element references
 const formRef = document.querySelector('.search__form');
 const resultsRef = document.querySelector('.results');
-const favouritesRef = document.querySelector('.favourites');
+const favouritesRef = document.querySelector('.favs');
 
 //Initialize: local storage
 const myStorage = window.localStorage;
 
 //Initialize: populate favourites
-favouritesRef.innerHTML = myStorage.getItem('favourites');
-
-//Initialize: tick checkboxes of favourites
+refreshFavourites();
 
 //Event listeners: click on document
 document.addEventListener('click', event => {
     if (event.target.matches('.result')) fetchDetails(event.target.getAttribute('data-id'));
     if (event.target.matches('.nav__prev')) prevButtonPressed(event);
     if (event.target.matches('.nav__next')) nextButtonPressed(event);
+    if (event.target.matches('.fav__up')) moveFavouriteUp(event.target.getAttribute('data-id'));
+    if (event.target.matches('.fav__down')) moveFavouriteDown(event.target.getAttribute('data-id'));
+    if (event.target.matches('.fav__delete')) removeFromFavourites(event.target.parentNode.getAttribute('data-id'));
 });
 
 //Event listeners: submit on form
 formRef.addEventListener('submit', submitSearch);
 
-//Event listeners: change on document for checkboxes
+//Event listeners: favourite checkboxes
 document.addEventListener('change', event => {
     if (event.target.matches('.result__checkbox')) {
         if (event.target.checked) addToFavourites(event.target.getAttribute('data-id'));
@@ -32,25 +33,57 @@ document.addEventListener('change', event => {
     }
 });
 
+function moveFavouriteUp(imdbID) {
+    let currentFavs = JSON.parse(myStorage.favourites);
+}
+
+function moveFavouriteDown(imdbID) {
+    let currentFavs = JSON.parse(myStorage.favourites);
+}
+
+//Functions: reload favourites HTML elements from local storage
+function refreshFavourites() {
+    if (JSON.parse(myStorage.favourites) !== null) {
+        favouritesRef.innerHTML = '';
+        JSON.parse(myStorage.favourites).forEach(fav => {
+            favouritesRef.appendChild(generateFavourite(fav.imdbID, fav.Title));
+            const checkbox = document.querySelector(`[data-id=${fav.imdbID}] .result__checkbox`);
+            if (checkbox !== null ) checkbox.checked = true;
+        });
+    }
+}
+
+//Functions: generate HTML element for favourite
+function generateFavourite(imdbID,title) {
+    const favourite = document.createElement('div');
+    favourite.setAttribute('class','fav');
+    favourite.setAttribute('data-id',imdbID);
+    favourite.innerHTML =  `<i>${title}</i>
+                            <i class="fav__up">Up</i>
+                            <i class="fav__down">Down</i> 
+                            <i class="fav__delete">Delete</i>`;
+    return favourite;
+}
+
 //Functions: Add to favourites
 function addToFavourites(imdbID) {
     const body = JSON.parse(myStorage.getItem('body'));
-    movieData = body.Search.filter(item => item.imdbID === imdbID)[0];
-    console.log(movieData);
-    const favourite = document.createElement('li');
-    favourite.setAttribute('class','favourite');
-    favourite.setAttribute('id',imdbID);
-    favourite.innerHTML = movieData.Title;
-    favouritesRef.appendChild(favourite);
-    myStorage.setItem('favourites',favouritesRef.innerHTML);
+    const movieData = body.Search.filter(item => item.imdbID === imdbID)[0];
+    let currentFavs = JSON.parse(myStorage.favourites);
+    if (currentFavs === null) currentFavs = [movieData];
+    else currentFavs.push(movieData);
+    myStorage.favourites = JSON.stringify(currentFavs);
+    refreshFavourites();
 }
 
 //Functions: Remove from favourites
 function removeFromFavourites(imdbID) {
-    console.log(`removed ${imdbID} from favourites`);
-    const favouriteToRemove = document.querySelector(`.favourites #${imdbID}`);
-    favouritesRef.removeChild(favouriteToRemove);
-    myStorage.setItem('favourites',favouritesRef.innerHTML);
+    let currentFavs = JSON.parse(myStorage.favourites);
+    currentFavs = currentFavs.filter(item => (item.imdbID !== imdbID));
+    myStorage.favourites = JSON.stringify(currentFavs);
+    const favToUncheck = document.querySelector(`[data-id=${imdbID}] .result__checkbox`);
+    if (favToUncheck !== null) favToUncheck.checked = false;
+    refreshFavourites();
 }
 
 //Functions: search submit
@@ -94,8 +127,7 @@ function fetchDetails(imdbID) {
 
 //Functions: render detailed movie information
 function renderDetails(body) {
-    console.log(body);
-    const detailRef = document.querySelector(`#${body.imdbID} .detail`);
+    const detailRef = document.querySelector(`[data-id=${body.imdbID}] .detail`);
     detailRef.innerHTML = `<p class='detail-rated'>${body.Rated}</p>
                         <p class='detail-runtime'>${body.Runtime}</p>
                         <p class='detail-genre'>${body.Genre}</p>
@@ -108,7 +140,6 @@ function renderDetails(body) {
 
 //Functions: fetch top level movie information
 function fetchResults(APIQuery) {
-    console.log(APIQuery);
     fetch(APIQuery)
     .then(response => {
         if (!response.ok) throw response;
@@ -119,27 +150,18 @@ function fetchResults(APIQuery) {
 
 //Functions: render search results
 function renderResults(body) {
-    console.log(body);
     myStorage.setItem('body',JSON.stringify(body));
     resultsRef.innerHTML = '';
     body.Search.forEach(item => {
         const result = document.createElement('article');
         result.setAttribute('class','result');
-        result.setAttribute('id',item.imdbID);
         result.setAttribute('data-id',item.imdbID);
         result.innerHTML = `<h1 data-id=${item.imdbID} class='result result__title'>${item.Title}</h1>
+                            <input data-id=${item.imdbID} class='result result__checkbox' type='checkbox'>
                             <h2 data-id=${item.imdbID} class='result result__year'>${item.Year}</h2>
                             <img data-id=${item.imdbID} class='result result__poster' src=${item.Poster}>
                             <div class='detail'></div>`;
         resultsRef.appendChild(result);
-        const favCheckbox = document.createElement('input');
-        favCheckbox.setAttribute('data-id',item.imdbID);
-        favCheckbox.setAttribute('class', 'result result__checkbox');
-        favCheckbox.setAttribute('type', 'checkbox');
-        const favouriteRef = document.querySelectorAll('.favourite');
-        favouriteRef.forEach(fav => {
-            if (fav.getAttribute('id') === item.imdbID) favCheckbox.checked = true;
-        });
-        resultsRef.appendChild(favCheckbox);
     });
+    refreshFavourites();
 }
