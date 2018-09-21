5a38500efef464ce68f63f317a12c2d8c86b8bef
Roland Levy <rolandjlevy@gmail.com>
2018-09-21 12:41:03 +0100

    Listing search results with link to page based on imdbID. Starting pagination

--- src/index.js
+++ src/index.js
@@ -1,38 +1,57 @@
+
 const omdUrl = "http://www.omdbapi.com/?type=movie";
 const apiKey = "2454706d";
+const mainNode = document.querySelector(".content");
+const form = document.querySelector(".form");
+const searchInput = document.querySelector(".search");
 
-const getFilms = function(url) {
-    fetch(url)
+const getFilms = function() {
+    const urlObject = getUrlFromSearch();
+    if (!urlObject.searchInput) return;
+    fetch(urlObject.url)
     .then(response => response.json())
     .then(body => {
-        displayFilms(body);
-        populateResults(body);
+        listenForSearchInput();
+        showSearchResults(body, urlObject.searchInput);
     })
 }
 
-const displayFilms = function (){
-    const form = document.querySelector(".form");
+const showSearchResults = function (body, input) {
+    if (!body) return;
+    mainNode.innerHTML = '';
+    let html = `<div><b><i>${body.totalResults} results for "${input}"</i></b></div><br>`;
+    body.Search.forEach(film => {
+        const linkUrl = getUrlFromResultLink(film.imdbID);
+        html += `<div><b>Title:</b> "${film.Title}", <b>ID:</b> <a href="${linkUrl}" target="_blank">${film.imdbID}</a></div>`;
+    });
+    html += `<br><div><a href="#">Pages 1 - 10</a></div>`;
+    mainNode.innerHTML = html;
+}
+
+const listenForSearchInput = function (){
     form.addEventListener('submit', event => {
         event.preventDefault();
-        const url = getURL();
-        getFilms(url);
+        getFilms();
     });
 }
 
-const getURL = function () {
-    const searchInput = document.querySelector(".search");
-    // const title = searchInput.value ? searchInput.value : "";
-    return `${omdUrl}&t=${searchInput.value}&apikey=${apiKey}`;
+const getUrlFromSearch = function () {
+    if (searchInput.value.length === 0) return false;
+    const url = `${omdUrl}&s=${searchInput.value}&apikey=${apiKey}`;
+    // console.log(url);
+    return {"url": url, "searchInput": searchInput.value};
 }
 
-const populateResults = function (body) {
-    const mainNode = document.querySelector(".content");
-    mainNode.innerHTML = '';
-    const keys = Object.keys(body);
+const getUrlFromResultLink = function (id) {
+    return `${omdUrl}&i=${id}&apikey=${apiKey}`;
+}
+
+const populateSelectedFilm = function (film) {
+    const keys = Object.keys(film);
     let html = ""
     keys.forEach(key => {
-        html += `<div><b>${key}</b>: ${body[key]}</div>`;
-    })
-    mainNode.innerHTML = html;
+        html += `<div><b>${key}</b>: ${film[key]}</div>`;
+    });
+    // console.log(html);
 }
-displayFilms();
+listenForSearchInput();
