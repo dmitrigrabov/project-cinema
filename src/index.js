faddc2e7259c6103ad23be409d209511d7c45f12
Roland Levy <rolandjlevy@gmail.com>
2018-09-21 22:29:08 +0100

    still working on pagination

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 const baseUrl = "http://www.omdbapi.com/?apikey=edd66bb"; //"apiKey=2454706d";
+const contentNode = document.querySelector(".content");
 const resultsHeaderNode = document.querySelector(".results-header");
 const resultsListNode = document.querySelector(".results-list");
 const resultsPagesNode = document.querySelector(".results-pages");
 const resultsShowingNode = document.querySelector(".results-showing");
 const form = document.querySelector(".form");
 const searchInput = document.querySelector(".search");
-searchInput.value = "sorry";
+searchInput.value = "Cars";
 let currentPage = 1;
-let prevActive = null;
+let prevThumb = null;
+let loaded = 0;
 
 function getMoviesFromSearch (url){
     if (searchInput.value.length === 0) return;
+    resultsListNode.innerHTML = 'Loading...';
     return fetch(url)
     .then(response => response.json())
     .then(body => {
-        searchHeaderMessage(body);
-        pageNumbers(body);
+        if (loaded === 0) { 
+            searchHeaderMessage(body);
+            pageNumbers(body);
+            loaded = 1;
+        }
         return body.Search
     })
     .catch(error => console.log(error))
@@ -24,19 +30,19 @@ function getMoviesFromSearch (url){
 // render film list
 function renderFilmList(filmList) {
     return filmList.map(film => {
-        return `<div><b>Title:</b> ${film.Title}, <b>ID:</b><a href=${baseUrl}&i=${film.imdbID} target="_blank">${film.imdbID}</a></div>`;
+        return `<div><b>Title:</b> ${film.Title}, <b>ID: </b><a href=${baseUrl}&i=${film.imdbID} target="_blank">${film.imdbID}</a></div>`;
     }).join('');
 }
 
 function searchHeaderMessage (body) {
-    let html = `<div><b><i>${body.totalResults} results for "${searchInput.value}"</i></b></div><br>`;
+    let html = `<div><b><i>${body.totalResults} results for "${searchInput.value}"</i></b></div>`;
     resultsHeaderNode.innerHTML = '';
     resultsHeaderNode.innerHTML = html;
 }
 
 const pageNumbers = function (body) {
     const totalPageLinks = Math.floor(body.totalResults / 10) + 1;
-    resultsShowingNode.innerHTML = `<br><div><i>Showing page ${currentPage} of ${totalPageLinks}</i></div><br><div>`;
+    resultsShowingNode.innerHTML = `<div><i>Showing page ${currentPage} of ${totalPageLinks}</i></div><div>`;
     let html = ``;
     for (let i=1; i<=totalPageLinks; i++) {
         const active = i === 1 ? " active" : "";
@@ -44,6 +50,7 @@ const pageNumbers = function (body) {
     }
     resultsPagesNode.innerHTML = "";
     resultsPagesNode.innerHTML = html;
+    document.querySelector(".active").click();
 }
 
 function getMoviesFromPagination (url){
@@ -56,33 +63,29 @@ function getMoviesFromPagination (url){
 function renderResultsFromSearch(page) {
     const url = `${baseUrl}&s=${searchInput.value}&page=${page}`;
     getMoviesFromSearch(url).then(filmList => {
-        resultsListNode.innerHTML = '';
         resultsListNode.innerHTML = renderFilmList(filmList);
     });
 }
 
 const newSearchPageFetch = function (page, totalPageLinks){
-    // let active = document.querySelector(".active");
-    // active.className = "page-number-link active"
-    // if (prevActive) { 
-    //     prevActive.className = "page-number-link";
-    //     prevActive = active;
-    // }
-    // const prevActive = document.querySelector(".active");
-    // if (prevActive !== null) {
-    //     prevActive.classList.remove("active")
-    //     event.target.classList.add("active")
-    // }
-    // console.log(active);
-    // const pageNumberlinks = document.querySelectorAll(".page-number-link");
-    // pageNumberlinks.addEventListener();
-    resultsShowingNode.innerHTML = `<br><div><i>Showing page ${page} of ${totalPageLinks}</i></div><br><div>`;
+    resultsShowingNode.innerHTML = `<div><i>Showing page ${page} of ${totalPageLinks}</i></div>`;
     renderResultsFromSearch(page);
     currentPage = page;
 }
 
+contentNode.addEventListener('click', event => {
+    if (event.target.matches('.page-number-link')){
+        const prevThumb = document.querySelector(".active");
+        if (prevThumb !== null) {
+            prevThumb.classList.remove("active")
+            event.target.classList.add("active")
+        }
+    }
+});
+
+
 form.addEventListener('submit', event => {	
-    currentPage = 1;    
+    currentPage = 1;   
     event.preventDefault();
     renderResultsFromSearch(currentPage);
 });
