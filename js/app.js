13a31c86422ecfde0c8385e5ad383478f5871c6a
Mariusz Sygnowski <sygnowski.m@gmail.com>
2018-09-21 14:20:06 +0100

    Title, year and poster is displayed after typed in search box

--- js/app.js
+++ js/app.js
@@ -3,10 +3,35 @@ const form = document.querySelector('#search'),
     searchButton = form.querySelector('.search__btn'),
     results = document.querySelector('.results');
 
+let titleMovies = '',
+    inputValue = '';
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-
-    results.innerHTML = `
-    <p>${searchInput.value}</p>
-    `
+    inputValue = searchInput.value;
+    runFetch(inputValue);
 });
+
+function runFetch(inputValue) {
+    fetch(`http://www.omdbapi.com/?apikey=f899a3c1&s=${inputValue}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((body) => {
+            body.Search.forEach(e => {
+                let searchResult = document.createElement('div');
+                searchResult.className = 'results__searchResult';
+                searchResult.innerHTML = `
+                    <p class="results__searchResult__title">${e.Title}</p>
+                    <p class="results__searchResult__year">${e.Year}</p>
+                    <img class="results__searchResult__poster" src="${e.Poster}"/>
+                    `;
+                results.append(searchResult);
+            });
+
+        })
+        .catch((error) => {
+            console.log('Server failed to return data: ' + error);
+        });
+}
+
