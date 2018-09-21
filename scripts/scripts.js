b1c0db105ddaf307bb9e6569b93f334e98c8d1bb
DanGRT <d.geraghty06@gmail.com>
2018-09-21 17:28:15 +0100

    started to make accomodations for API returning errors, final commit of day

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,50 +2,49 @@
 
 //http://www.omdbapi.com/?apikey=210776d9&[searchparameters]
 
+//feature idea: News API to check for stories about film
 
-const bodyElement = document.querySelector("body")
-const searchTextElement = document.querySelector(".search__text")
-const searchResultsElement = document.querySelector(".results")
-const filmDisplayElement = document.querySelector(".film-display")
-const modal = document.querySelector(".modal")
-const displaySearchButton = document.querySelector(".search__display-bar")
-const searchBarElement = document.querySelector(".nav__search")
-const homeNavBar = document.querySelector(".nav__home")
-
+const bodyElement = document.querySelector("body");
+const searchTextElement = document.querySelector(".search__text");
+const searchResultsElement = document.querySelector(".results");
+const filmDisplayElement = document.querySelector(".film-display");
+const modal = document.querySelector(".modal");
+const displaySearchButton = document.querySelector(".search__display-bar");
+const searchBarElement = document.querySelector(".nav__search");
+const homeNavBar = document.querySelector(".nav__home");
 
 bodyElement.addEventListener("submit", event => {
-  event.preventDefault()
-  if (event.target.matches(".search")){
-    modal.style.display = "block"
-    apiUrls.updateURL("s", searchTextElement.value)
-    apiUrls.fetchResults(apiUrls.getURL())
+  event.preventDefault();
+  if (event.target.matches(".search")) {
+    modal.style.display = "block";
+    apiUrls.updateURL("s", searchTextElement.value);
+    apiUrls.fetchResults(apiUrls.getURL());
   }
-})
+});
 
 //closest instead of matches, pay attention in case of bugs
 bodyElement.addEventListener("click", event => {
-  if (event.target.closest(".search__result")){
-    modal.style.display = "none"
-    searchResultsElement.innerHTML = ""
-    filmDisplayElement.innerHTML = ""
-    toggleNavBar()
-    apiUrls.updateMovieURL("i", event.target.dataset.id)
-    apiUrls.fetchMovie(apiUrls.getMovieURL())
+  if (event.target.closest(".search__result")) {
+    modal.style.display = "none";
+    searchResultsElement.innerHTML = "";
+    filmDisplayElement.innerHTML = "";
+    toggleNavBar();
+    apiUrls.updateMovieURL("i", event.target.dataset.id);
+    apiUrls.fetchMovie(apiUrls.getMovieURL());
   }
-  if (event.target.matches(".search__display-bar")){
-    toggleNavBar()
-
+  if (event.target.matches(".search__display-bar")) {
+    toggleNavBar();
   }
-})
+});
 
 //look into actual toggle functionality with BEM
-function toggleNavBar(){
-  if (homeNavBar.style.display === "flex"){
-     searchBarElement.style.display = "flex"
-     homeNavBar.style.display = "none"
-  }else{
-    searchBarElement.style.display = "none"
-    homeNavBar.style.display = "flex"
+function toggleNavBar() {
+  if (homeNavBar.style.display === "flex") {
+    searchBarElement.style.display = "flex";
+    homeNavBar.style.display = "none";
+  } else {
+    searchBarElement.style.display = "none";
+    homeNavBar.style.display = "flex";
   }
 }
 
@@ -61,78 +60,78 @@ const apiUrls = {
     plot: ""
   },
 
-
-
   updateURL: function(parameter, update) {
     this.searchParameters[parameter] = `&${parameter}=${update}`;
-
   },
 
-  updateMovieURL: function(parameter, update){
+  updateMovieURL: function(parameter, update) {
     this.movieParameters[parameter] = `&${parameter}=${update}`;
-
   },
 
   getURL: function() {
-  const customURL = `http://www.omdbapi.com/?apikey=210776d9${this.searchParameters.s}`
-  return customURL
-},
-
-  getMovieURL: function(){
-    return `http://www.omdbapi.com/?apikey=210776d9${this.movieParameters.i}`
+    const customURL = `http://www.omdbapi.com/?apikey=210776d9${
+      this.searchParameters.s
+    }`;
+    return customURL;
   },
 
-  fetchResults: function(apiURL){
-    fetch(apiURL)
-    .then(response => response.json())
-    .then(body => {
-      body.Search.forEach(result => {
-        searchResultsElement.appendChild(searchTemplate(result))
-
-      })
-    })
+  getMovieURL: function() {
+    return `http://www.omdbapi.com/?apikey=210776d9${this.movieParameters.i}`;
   },
 
-  fetchMovie: function(apiURL){
+  fetchResults: function(apiURL) {
     fetch(apiURL)
-    .then(response => response.json())
-    .then(body => {
-      filmDisplayElement.appendChild(fullFilmTemplate(body))
+      .then(response => response.json())
+      .then(body => {
+        if (body.hasOwnProperty("Error")) {
+          //TODO: needs to be filled out
+          console.log("error");
+        } else {
+          body.Search.forEach(result => {
+            searchResultsElement.appendChild(searchTemplate(result));
+          });
+        }
+      });
+  },
 
+  fetchMovie: function(apiURL) {
+    fetch(apiURL)
+      .then(response => response.json())
+      .then(body => {
+        filmDisplayElement.appendChild(fullFilmTemplate(body));
+      });
+  }
+};
 
-    })
+const pageHandlers = {};
 
-}
-
-}
-
-const pageHandlers = {
-
-}
-
-function searchTemplate (result){
-  const searchResultElement = document.createElement("div")
+function searchTemplate(result) {
+  const searchResultElement = document.createElement("div");
   const template = `
     <div class="search__result" data-id=${result.imdbID}>
-    <img class="result__poster" src=${result.Poster} data-id=${result.imdbID}/>
-    <h4 class="result__title" data-id=${result.imdbID}>${result.Title}</h4>
-    <h5 class="result__year" data-id=${result.imdbID}>(${result.Year})</h5>
-    <h5 class="result__type" data-id=${result.imdbID}>${result.Type}</h5>
+      <img class="result__poster" src=${result.Poster} data-id=${result.imdbID}/>
+      <h4 class="result__title" data-id=${result.imdbID}>${result.Title}</h4>
+      <h5 class="result__year" data-id=${result.imdbID}>(${result.Year})</h5>
+      <h5 class="result__type" data-id=${result.imdbID}>${result.Type}</h5>
     </div>
-  `
-  searchResultElement.innerHTML = template
-  return searchResultElement
+  `;
+  searchResultElement.innerHTML = template;
+  return searchResultElement;
 }
 
-function fullFilmTemplate (result){
-  const filmInfoElement = document.createElement("div")
+function fullFilmTemplate(result) {
+  const filmInfoElement = document.createElement("div");
   const template = `
     <div class="film-display">
-    <h2>${result.Title}</h2>
-    <h4>${result.Actors}<h4>
-    <p>${result.Plot}<p>
+      <div class="film-display__header">
+        <h2>${result.Title}</h2>
+        <h2>(${result.Year})</h2>
+      </div>
+      <img class="film-display__poster" src=${result.Poster}/>
+      <h4>Cast: ${result.Actors}<h4>
+      <p>${result.Plot}<p>
     </div>
-  `
-  filmInfoElement.innerHTML = template
-  return filmInfoElement
+  `;
+  filmInfoElement.innerHTML = template;
+  return filmInfoElement;
 }
