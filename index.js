f4fdd537c58e4069e9f7d16183982794a2c37f7e
ljsikuade <40580944+ljsikuade@users.noreply.github.com>
2018-09-22 19:00:54 +0100

    working on reviews

--- index.js
+++ index.js
@@ -65,29 +65,82 @@ function typeSearchFilm(filmName) {
     .then(body => createInfoPanel(body));
 }
 
-function createInfoPanel(typeSearchedObject) {
+async function createInfoPanel(typeSearchedObject) {
+  const movieID = await getMovieIDForReview(typeSearchedObject);
+  const review = await getReview(movieID);
   const infoPanel = document.createElement("div");
   infoPanel.className = "info__panel";
   infoPanel.innerHTML =
     //prettier-ignore
-    `<button class="info__panel__close" onclick ="closeInfoPanel(this)">x</button>
-    <img src="${typeSearchedObject.Poster}">
-    <h1 class="info__panel__title">${typeSearchedObject.Title}</h1>
-    <h1 class="info__panel__date">${typeSearchedObject.Year}</h1>
-    <p class="info__panel__description">${typeSearchedObject.Plot}</p>
-    <p class="info__panel__director">${typeSearchedObject.Director}</p>
-    <p class="info__panel__starring">${typeSearchedObject.Actors}</p>
-    <p class="genre">${typeSearchedObject.Genre}</p>
+    `<div class= "close">
+    <button class="info__panel__close" onclick ="closeInfoPanel(this)">x</button>
+    </div>
+    <div class="img__title__date__genre">
+      <img src="${typeSearchedObject.Poster}">
+      <h1 class="info__panel__title">${typeSearchedObject.Title}</h1>
+      <h1 class="info__panel__date">${typeSearchedObject.Year}</h1>
+      <p class="genre">${typeSearchedObject.Genre}</p>
+    </div>
+    <div class="desc">
+      <p class="info__panel__description">${typeSearchedObject.Plot}</p>
+      <h2>Top Review:</h2>
+      <p class="info__panel__review">${await review.content}</p>
+      <p class="reviewer">${await review.author}</p>
+    </div>
+    <div class="director__starring">
+      <p class="info__panel__director">Directed by: ${typeSearchedObject.Director}</p>
+      <p class="info__panel__starring">Starring: ${typeSearchedObject.Actors}</p>
+    </div>
+    
     <div class="rating">
-        <span class="fa fa-star checked"></span>
-        <span class="fa fa-star checked"></span>
-        <span class="fa fa-star checked"></span>
-        <span class="fa fa-star"></span>
-        <span class="fa fa-star"></span>
+        <span class="stars">
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star"></span>
+          <span class="fa fa-star"></span>
+        </span>
     </div>`;
-
+  //Will need to append it into view.
   const filmContainer = document.querySelector(".display__film");
   filmContainer.appendChild(infoPanel);
 }
 
-const closeInfoPanel = closeButton => (closeButton.parentNode.innerHTML = "");
+const closeInfoPanel = closeButton =>
+  (closeButton.parentNode.parentNode.innerHTML = "");
+
+async function getMovieIDForReview(filmObject) {
+  let movieDBBaseURI = `https://api.themoviedb.org/3/search/movie?api_key=c1eda2d27f7a73d8ca633b6936e5b012&query=`;
+
+  const response = await fetch(`${movieDBBaseURI}${filmObject.Title}`);
+  const movieData = await response.json();
+
+  const movie = movieData.results.reduce((acc, current) => {
+    if (current.title === filmObject.Title) {
+      acc = current.id;
+    } else {
+      acc = "Review not found...";
+    }
+    return acc;
+  });
+  return movie;
+}
+async function getReview(id) {
+  const fullReview = {};
+  //prettier-ignore
+  const response = await fetch(`https://api.themoviedb.org/3/movie/${id}/reviews?api_key=c1eda2d27f7a73d8ca633b6936e5b012`)
+  const reviews = await response.json();
+  const firstReview = await reviews.results[0];
+  await Object.assign(fullReview, firstReview);
+
+  return await fullReview;
+}
+
+// function youMayAlsoLike(genre, actors, director, writer){
+//     //search for films/tv with the same genre, actors and director
+//     //fail? search for films/tv with same genre and actors
+//     //fail? search for films with same genre and writer
+//     fetch(`${baseUrlSearch`){
+
+//     }
+// }
