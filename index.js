dd920d27a339754d444d127378867d23c4361026
ljsikuade <40580944+ljsikuade@users.noreply.github.com>
2018-09-23 17:36:14 +0100

    project completed but buggy

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ formInput.addEventListener("submit", function(searchEvent) {
   refineUrl(searchInput.value);
 });
 
-const refineUrl = search => fetchRefined(`${baseUrlSearch}${search}`);
+const refineUrl = search => {
+  let pageNum = 1;
+  fetchRefined(`${baseUrlSearch}${search}&page=${pageNum}`);
+  pageNum++;
+  nextpage(`${baseUrlSearch}${search}`, pageNum);
+};
 
 function fetchRefined(url) {
   fetch(url)
@@ -39,19 +44,41 @@ const appendIntoDocument = movie => {
   filmContainer.appendChild(movie);
 };
 
+function nextpage(search, pageNum) {
+  const next = document.createElement("button");
+  next.className = "next__page";
+  next.textContent = "Next Page";
+
+  const header = document.querySelector("header");
+  header.appendChild(next);
+  next.addEventListener("click", function(event) {
+    const films = document.querySelector(".films");
+    films.innerHTML = "";
+    const nextUrl = `${search}&page=${pageNum++}`;
+    fetchRefined(nextUrl);
+  });
+}
+
 ///////////////////////////////////////////////////////
 
 filmContainer.addEventListener("click", function(event) {
+  //prettier-ignore
   if (event.target.className === "more__info") {
     /*We need the parent node to (more readably) access
         the H2 tag.                                     */
     const superParent = event.target.parentNode.parentNode;
     const filmName = pullFilmName(superParent.childNodes);
     typeSearchFilm(filmName);
+    //For mobile.
+  }else if(event.target.className === "movie__img"){
+    const mobileSuperParent = event.target.parentNode;
+    const mobileFilmName = pullFilmName(mobileSuperParent.childNodes);
+    typeSearchFilm(mobileFilmName);
   }
 });
 
 const pullFilmName = childElementArray => {
+  console.log(childElementArray.length);
   return childElementArray[2].textContent;
 };
 
@@ -66,14 +93,19 @@ function typeSearchFilm(filmName) {
 }
 
 async function createInfoPanel(typeSearchedObject) {
+  //Specify that you will wait for the promise to resolve.
   const movieID = await getMovieIDForReview(typeSearchedObject);
   const review = await getReview(movieID);
+  //Currently not operating as intended.
+  let mobileClose = "";
+  screen.width < 700 ? (mobileClose = "Close") : (mobileClose = "x");
+
   const infoPanel = document.createElement("div");
   infoPanel.className = "info__panel";
   infoPanel.innerHTML =
     //prettier-ignore
     `<div class= "close">
-    <button class="info__panel__close" onclick ="closeInfoPanel(this)">x</button>
+    <button class="info__panel__close" onclick ="closeInfoPanel(this)">${mobileClose}</button>
     </div>
     <div class="img__title__date__genre">
       <img src="${typeSearchedObject.Poster}">
@@ -84,8 +116,8 @@ async function createInfoPanel(typeSearchedObject) {
     <div class="desc">
       <p class="info__panel__description">${typeSearchedObject.Plot}</p>
       <h2>Top Review:</h2>
-      <p class="info__panel__review">${await review.content}</p>
-      <p class="reviewer">${await review.author}</p>
+      <p class="review">${review.content}</p>
+      <p class="review__author">${review.author}</p>
     </div>
     <div class="director__starring">
       <p class="info__panel__director">Directed by: ${typeSearchedObject.Director}</p>
@@ -104,10 +136,12 @@ async function createInfoPanel(typeSearchedObject) {
   //Will need to append it into view.
   const filmContainer = document.querySelector(".display__film");
   filmContainer.appendChild(infoPanel);
+  infoPanel.scrollIntoView();
 }
 
-const closeInfoPanel = closeButton =>
-  (closeButton.parentNode.parentNode.innerHTML = "");
+const closeInfoPanel = closeButton => {
+  closeButton.parentNode.parentNode.remove();
+};
 
 async function getMovieIDForReview(filmObject) {
   let movieDBBaseURI = `https://api.themoviedb.org/3/search/movie?api_key=c1eda2d27f7a73d8ca633b6936e5b012&query=`;
@@ -115,25 +149,27 @@ async function getMovieIDForReview(filmObject) {
   const response = await fetch(`${movieDBBaseURI}${filmObject.Title}`);
   const movieData = await response.json();
 
-  const movie = movieData.results.reduce((acc, current) => {
-    if (current.title === filmObject.Title) {
-      acc = current.id;
-    } else {
-      acc = "Review not found...";
+  const movie = movieData.results.find(item => {
+    if (item.title === filmObject.Title) {
+      //console.log(item.id);
+      return item;
     }
-    return acc;
   });
-  return movie;
+  return movie.id;
 }
 async function getReview(id) {
-  const fullReview = {};
   //prettier-ignore
   const response = await fetch(`https://api.themoviedb.org/3/movie/${id}/reviews?api_key=c1eda2d27f7a73d8ca633b6936e5b012`)
   const reviews = await response.json();
   const firstReview = await reviews.results[0];
-  await Object.assign(fullReview, firstReview);
-
-  return await fullReview;
+  if (firstReview != undefined) {
+    return firstReview;
+  } else {
+    return {
+      content: "No review found for this production.",
+      author: `See for yourself at themoviedb.org, ID: ${id}`
+    };
+  }
 }
 
 // function youMayAlsoLike(genre, actors, director, writer){
