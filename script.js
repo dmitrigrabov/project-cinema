995f7ac19da1b3d9fca68f3cc52ff415c93b4e29
chrisphillers <chrisphillers@gmail.com>
2018-09-23 19:18:27 +0100

    Trailer functionality

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 //HTML Element Targets
 const parentSearchResult = document.querySelector(".results");
 const searchResultNode = document.createElement("li"); 
-const extraInfo =[];
+const searchFunc = document.querySelector(".search__form");
 const parentMoreInfo = document.querySelector(".more__info");
+const moreInfoNode = document.createElement("li");
+let trailerClip = '';
 
 
 //I could hardcode this in the html - leaving in for now.
@@ -13,7 +15,7 @@ parentSearchResult.appendChild(searchResultNode);
 
 
 //Search Function
-const searchFunc = document.querySelector(".search__form");
+
 searchFunc.addEventListener("submit", function(event) {
     event.preventDefault();
     console.log(event.target['0'].value);
@@ -40,12 +42,12 @@ function IMDBData(search) {
     //Event listener of individual listed movies - sends URL to fetch for more info
 
     const filmSelector = document.querySelector(`[data-imdb=${movies.imdbID}]`);
-              filmSelector.addEventListener("click", event => {
-                console.log(movies.imdbID);
+        filmSelector.addEventListener("click", event => {
                 let movieURL = `http://www.omdbapi.com/?i=${movies.imdbID}&apikey=73071eff`;
                 fetchMoreInfo(movieURL);
-                console.log(movieURL);
-              });
+                let trailerURL = `https://api.themoviedb.org/3/movie/${movies.imdbID}/videos?api_key=8aed3c92a5c6ef5e792ffaf51ac22616&language=en-US`
+                fetchTrailer(trailerURL);  
+            });
             });
            }
 
@@ -57,6 +59,11 @@ function notFound(){
     searchResultNodes.innerHTML = 
         `<div>help me</div>`;
     parentSearchResult.appendChild(searchResultNodes);    
+};
+
+
+function notFoundTrailer() {
+//TO DO
 };
 
 
@@ -85,6 +92,42 @@ function fetchIMDB(url){
 
 }
 
+//Trailer fetch
+
+function fetchTrailer(trailerURL){
+    
+    fetch(trailerURL) 
+    .then(function(response) {
+        
+        return response.json();
+    })
+    .then(function(result){
+     
+        // If unknown result
+        if (result.Response === 'False') {
+            // parentSearchResult.innerHTML = "";
+            console.log(result)
+            notFoundTrailer(); 
+        }
+        else
+        {   
+            // parentSearchResult.innerHTML = "";
+            trailerVideo(result);
+            console.log(result)
+        }
+    });
+
+}
+
+// fetchTrailer();
+function trailerVideo(result){
+trailerClip = `https://www.youtube.com/watch?v=${result.results[0].key}`;
+};
+console.log(trailerClip);
+// console.log(fetchTrailer());
+
+
+
 //MORE INFO APPEARS UPON CLICK - FETCH
 
 function fetchMoreInfo(movieURL){ //default for now
@@ -110,7 +153,7 @@ function fetchMoreInfo(movieURL){ //default for now
 
 function movieMore(finder){
 
-    const moreInfoNode = document.createElement("li");
+    
     moreInfoNode.innerHTML =
     `<div data-imdb=${finder.imdbID} value="${finder.imdbID}" class="results__wrapper">
     <img src="${finder.Poster}" alt="news image">
@@ -119,7 +162,7 @@ function movieMore(finder){
     <h2>${finder.Actors}</h2>
     <h2>${finder.Director}</h2>
     <h2>${finder.Plot}</h2>
-    <h2>${finder.rating}</h2>
+    <h2>${trailerClip}</h2>
     </div>`;
 
     parentMoreInfo.appendChild(moreInfoNode);
