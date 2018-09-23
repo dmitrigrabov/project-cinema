1935bf4bade449b4fc07338c5907ae88a483efdc
Mariusz Sygnowski <sygnowski.m@gmail.com>
2018-09-23 19:21:36 +0100

    when clicked on selected movie, poster is gone and in that place I fill with details from that movie

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const form = document.querySelector('#control__search'),
     searchInput = document.querySelector('.control__search__input'),
     results = document.querySelector('.results'),
     buttonsClass = document.querySelector('.buttons'),
+    currentPageClasses = document.querySelectorAll('.currentPage'),
     nextBtn = document.querySelector('.buttons__button-next'),
     prevBtn = document.querySelector('.buttons__button-prev');
 
@@ -15,7 +16,7 @@ let params = {
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    buttonsClass.style.display = 'grid';
+    buttonsClass.style.display = 'none';
     results.innerHTML = '';
     params.pageNumber = 1;
     params.totalPages = 0;
@@ -26,6 +27,7 @@ form.addEventListener('submit', (e) => {
 nextBtn.addEventListener('click', (e) => {
     e.preventDefault();
     results.innerHTML = '';
+    buttonsClass.style.display = 'none';
     if (params.pageNumber === params.totalPages) {
         params.pageNumber = 1;
     } else {
@@ -34,9 +36,11 @@ nextBtn.addEventListener('click', (e) => {
 
     runFetch();
 });
+
 prevBtn.addEventListener('click', (e) => {
     e.preventDefault();
     results.innerHTML = '';
+    buttonsClass.style.display = 'none';
     if (params.pageNumber === 1) {
         params.pageNumber = params.totalPages;
     } else {
@@ -54,6 +58,10 @@ function setUrlForEachMovieWithImdbIDNumber() { //I need imdbID to get full deta
     return `https://www.omdbapi.com/?i=${params.imdbID}&plot=full&apikey=${params.apiKey}&page=${params.pageNumber}`;
 }
 
+function fullDetails() {
+
+}
+
 function runFetch() {
     fetch(setUrlWithTypedSearch())
         .then((response) => {
@@ -62,7 +70,9 @@ function runFetch() {
         .then((body) => {
             params.totalPages = body.totalResults / 10; //divide by as as there is 10 results per page
             params.totalPages = Math.ceil(params.totalPages); //round up as I want to have all possible number of pages
-
+            currentPageClasses.forEach((currentPageClass) => {
+                currentPageClass.textContent = `page ${params.pageNumber} / ${params.totalPages}`;
+            });
             body.Search.forEach(e => {
                 params.imdbID = e.imdbID;
                 fetch(setUrlForEachMovieWithImdbIDNumber())
@@ -71,11 +81,13 @@ function runFetch() {
                     })
                     .then((body) => {
                         console.log(body);
+                        buttonsClass.style.display = 'block';
                         let movieParams = {
                             description: body.Plot,
                             poster: body.Poster,
                             title: body.Title,
-                            year: body.Year
+                            year: body.Year,
+                            actors: body.Actors
                         };
 
                         let searchResult = document.createElement('div');
@@ -87,17 +99,23 @@ function runFetch() {
                         if (movieParams.description === 'N/A') { //if there is no description I set
                             movieParams.description = '';
                         }
-                        if (movieParams.description.length > 290) { //if our description is longer than 290 characters I delete rest description and in that place insert 3 dots
-                            movieParams.description = `${movieParams.description.slice(0, 290)}...`;
-                        }
 
                         searchResult.innerHTML = `
+                            <span class="results__searchResult__title">${movieParams.title}</span>
                             <img class="results__searchResult__poster" src="${movieParams.poster}"/>
-                            <p class="results__searchResult__title">${movieParams.title}</p>
-                            <p class="results__searchResult__year">${movieParams.year}</p>
-                            <p class="results__searchResult__description">${movieParams.description}</p>
-                            <p class="emptyBox"></p>                        
+                            <span class="results__searchResult__year hide">${movieParams.year}</span>
+                            <span class="results__searchResult__description hide">${movieParams.description}</span>
+                            <span class="results__searchResult__actors hide">${movieParams.actors}</span>
                             `;
+                        const posterImg = searchResult.querySelector('.results__searchResult__poster');
+                        const description = searchResult.querySelector('.results__searchResult__description');
+                        const year = searchResult.querySelector('.results__searchResult__year');
+                        searchResult.addEventListener('click', () => {
+                            console.log(posterImg);
+                            description.classList.toggle('hide');
+                            year.classList.toggle('hide');
+                            posterImg.classList.toggle('hide');
+                        });
                         //using class emptyBox to push up all elements to the top box - without this emptyBox not all searchResult div will looks the same - sometimes I don't have description so it will be empty
                         results.append(searchResult);
                     })
